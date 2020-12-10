193678bea01769b6d4e83261c8c064236baba5af
endridulellari <dulellarie@yahoo.com>
2020-12-10 23:11:32 +0100

    Exposing Employees, Invoices and Locations pages.

--- src/main/java/com/cibus/cibusapp/models/Invoice.java
+++ src/main/java/com/cibus/cibusapp/models/Invoice.java
@@ -28,19 +28,12 @@ public class Invoice {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")	
 	private Date invoiceDate;
-	
-	@ManyToOne
-	@JoinColumn(name="invoicestatusid", insertable=false, updatable=false)	
-	private InvoiceStatus invoiceStatus;
-	private Integer invoicestatusid;
-	
-	@ManyToOne
-	@JoinColumn(name="clientid", insertable=false, updatable=false)	
-	private Client client;
-	private Integer clientid;
-	
-	private String remarks;	
+	private Integer invoiceNo;
+	private String invoicestatus;
+	private Integer qty;
+	private Integer unitprice;
+	private Integer total;
+	private String description;
 }
